9d97ae576fc74909f1283775b3e05ff2b8adaabe
Justin Johnson <justin@oxd.com>
2025-11-06 14:29:44 -0800

    DBC22-5017 Adding polygons to service areas

--- src/webapp/apps/organizations/models.py
+++ src/webapp/apps/organizations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.gis.db import models as gis
 
 from apps.events.models import BaseModel
 
@@ -13,3 +14,4 @@ class ServiceArea(BaseModel):
     name = models.CharField(max_length=128, blank=True, default='')
     sortingOrder = models.PositiveSmallIntegerField()
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=False, related_name='children')
+    geometry = gis.PolygonField(geography=True, srid=4326, blank=True, null=True)
