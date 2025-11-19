58f8d6121ad2159df361bc598f38eb2c1e3a0a18
ray <ray@oxd.com>
2025-11-18 16:21:13 -0800

    DBC22-5087: hide Users link from non-superusers

--- src/frontend/app/layout.jsx
+++ src/frontend/app/layout.jsx
@@ -105,7 +105,10 @@ export default function Layout() {
         {authContext.username &&
           <>
             <NavLink to="/events/">Events</NavLink>
-            <NavLink to="/users/">Users</NavLink>
+
+            {authContext.is_superuser &&
+              <NavLink to="/users/">Users</NavLink>
+            }
           </>
         }
 
