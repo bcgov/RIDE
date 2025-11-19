f2e893082a5c64ac068f99d5c81ba31162a7331c
ray <ray@oxd.com>
2025-11-19 15:32:52 -0800

    DBC22-5087: added display fields to django admin

--- src/webapp/apps/users/admin.py
+++ src/webapp/apps/users/admin.py
@@ -4,4 +4,8 @@ from .models import RIDEUser
 
 @admin.register(RIDEUser)
 class RIDEUserAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'username', 'email',
+        'first_name', 'last_name',
+        'is_staff', 'is_superuser',
+    )
