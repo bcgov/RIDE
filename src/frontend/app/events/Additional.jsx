96b196cefcad4256cd77e18e8debb9bdee894c7b
Justin Johnson <justin@oxd.com>
2026-02-05 12:32:07 -0800

    DBC22-5543 Increase text limit for additional messaging

--- src/frontend/app/events/Additional.jsx
+++ src/frontend/app/events/Additional.jsx
@@ -4,13 +4,13 @@ export default function AdditionalMessaging({ event, dispatch }) {
   const [text, setText] = useState(event.additional);
 
   const change = (e) => {
-    setText(e.target.value.substring(0, 200))
+    setText(e.target.value.substring(0, 400))
   }
 
   return <>
     <div className="title">
       <p><strong>Additional Messaging</strong> (optional)</p>
-      <p className={text.length === 200 ? 'bold' : ''}>{text.length}/200</p>
+      <p className={text.length === 400 ? 'bold' : ''}>{text.length}/400</p>
     </div>
     <textarea
       name="additional"
