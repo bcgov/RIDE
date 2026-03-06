b76a043882c4a592ac5183eeb85ae1e9ee6b50e0
ray <ray@oxd.com>
2026-03-06 14:54:57 -0800

    DBC22-5626: added validation for special characters that aren't punctuation

--- src/frontend/app/events/Additional.jsx
+++ src/frontend/app/events/Additional.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
 
-export default function AdditionalMessaging({ event, dispatch }) {
+export default function AdditionalMessaging({ event, dispatch, errors }) {
   const [text, setText] = useState(event.additional || '');
+  const error = errors?.additional;
 
   const change = (e) => {
     setText(e.target.value.substring(0, 400))
   }
 
   return <>
-    <div className="title">
-      <p><strong>Additional Messaging</strong> (optional)</p>
+    <div className={`title ${error ? 'error' : ''}`}>
+      <p>
+        <strong>Additional Messaging</strong> (optional)
+        { error && <span className="error-message">{error}</span>}
+      </p>
       <p className={text.length === 400 ? 'bold' : ''}>{text.length}/400</p>
     </div>
     <textarea
--- src/frontend/app/events/forms.jsx
+++ src/frontend/app/events/forms.jsx
@@ -36,6 +36,7 @@ import './forms.css';
 
 const ONE_HOUR = 1000 * 60 * 60;
 const ONE_WEEK = ONE_HOUR * 24 * 7
+const INVALID_ADDITIONAL_CHARACTERS = /[<>\[\]{}\\|~`!@#$%\^&*()_+=]/;
 
 function getLater(severity) {
   if (!severity) { return null; }
@@ -442,6 +443,10 @@ export default class EventForm extends Component {
       err['Conditions'] = 'Must include at least one';
     }
 
+    if (form.additional && INVALID_ADDITIONAL_CHARACTERS.test(form.additional)) {
+      err.additional = 'Contains unsupported special characters';
+    }
+
     if (form.type === 'Incident') {
       if (!form.timing.nextUpdate && !form.timing.endTime) {
         err['Manage Timing By'] = 'Must set one or both';
@@ -675,7 +680,7 @@ export default class EventForm extends Component {
 
                   { event.type &&
                     <div className="section additional">
-                      <AdditionalMessaging event={event} dispatch={dispatch} />
+                      <AdditionalMessaging event={event} dispatch={dispatch} errors={errors} />
                     </div>
                   }
 
