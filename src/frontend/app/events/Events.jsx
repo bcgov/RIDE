cdab0e74a04b0e1ce513c5af3fa0a18baa437f17
Justin Johnson <justin@oxd.com>
2026-03-18 13:54:57 -0700

    DBC22-5735 Searching active events

--- src/frontend/app/events/Events.jsx
+++ src/frontend/app/events/Events.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import { useSelector, useDispatch, useStore } from 'react-redux';
 import { useNavigate } from 'react-router';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencil } from '@fortawesome/pro-solid-svg-icons';
-import { faStopwatch } from '@fortawesome/pro-regular-svg-icons';
+import { faStopwatch, faXmark, faMagnifyingGlass } from '@fortawesome/pro-regular-svg-icons';
 import { formatDistanceToNowStrict } from 'date-fns';
 
 import { selectAllEvents } from '../slices/events.js';
@@ -15,6 +16,50 @@ import { getConditionIcon, getPlainIcon } from './icons';
 import { PHRASES_LOOKUP } from './references';
 
 import './Events.scss';
+
+/* Searches various event fields to find a match with term.
+ *
+ * Incrementally builds search fields so it can immediately return true on a
+ * match, avoiding subsequent checking.
+ */
+function matchEvent(event, term) {
+  term = term.toLowerCase();
+
+  if (event.id.toLowerCase().includes(term)) { return true; }
+
+  let roads = event?.location?.start?.name || '';
+  roads += event?.location?.end?.name || '';
+  if (event?.location?.start?.alias && event?.location?.start?.useAlias) {
+    roads += `;${event.location.start.alias}`;
+  }
+  if (event?.location?.end?.alias && event?.location?.end?.useAlias) {
+    roads += `;${event.location.end.alias}`;
+  }
+  if (roads.toLowerCase().includes(term)) { return true; }
+
+  let user = event?.user?.first_name || '';
+  user += `;${event?.user?.last_name || ''}`;
+  user += `;${event?.user?.username || ''}`;
+  user += `;${event?.user?.email || ''}`;
+  if (user.toLowerCase().includes(term)) { return true; }
+
+  let nearby = event?.location?.start?.nearby.filter((nearby) => nearby.include).map((nearby) => nearby.name).join(';') || '';
+  nearby += (event?.location?.end?.nearby.filter((nearby) => nearby.include).map((nearby) => nearby.name).join(';') || '');
+  if (event?.location?.start?.other && event?.location?.start?.useOther) {
+    nearby += `;${event.location.start.other}`;
+  }
+  if (event?.location?.end?.other && event?.location?.end?.useOther) {
+    nearby += `;${event.location.end.other}`;
+  }
+  if (nearby.toLowerCase().includes(term)) { return true; }
+
+  let phrases = PHRASES_LOOKUP[event?.details?.situation] || '';
+  phrases += event?.details?.category || '';
+  phrases += event?.impacts?.map((impact) => `;${impact.label}`) || '';
+  phrases += event?.conditions?.map((condition) => `;${condition.label}`) || '';
+  phrases += event?.restrictions?.map((restriction) => `;${restriction.label};${restriction.text}`) || '';
+  return phrases.toLowerCase().includes(term);
+}
 
 function Event({ event, goToFunc, dispatch, map, selected, areaLabel }) {
 
@@ -130,6 +175,8 @@ function Event({ event, goToFunc, dispatch, map, selected, areaLabel }) {
 const ONE_HOUR = 1000 * 60 * 60;
 
 export default function Events({ goToFunc, dispatch, map, current }) {
+  const [ search, setSearch ] = useState('');
+
   const events = useSelector(selectAllEvents);
   const serviceAreas = useSelector(selectServiceAreasIdLabel);
 
@@ -152,10 +199,38 @@ export default function Events({ goToFunc, dispatch, map, current }) {
     }
 
     return { ...event, delta, ending, };
-  }).filter((event) => event.delta < ONE_HOUR);
+  }).filter(
+    (event) => event.delta < ONE_HOUR
+  ).filter((event) => {
+    if (search && search.length > 2) {
+      return matchEvent(event, search);
+    }
+    return true;
+  });
+
   displayed.sort((a, b) => a.delta < b.delta ? -1 : 1);
+
   return (
     <div className='events-list'>
+      <div className='search'>
+        <div className='search-input'>
+          <input
+            type='text'
+            name='search'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+
+          <span className='search-icon'><FontAwesomeIcon icon={faMagnifyingGlass} /></span>
+        </div>
+
+        { search &&
+          <button type='button' onClick={() => setSearch('')}>
+            <FontAwesomeIcon icon={faXmark} />&nbsp;clear
+          </button>
+        }
+      </div>
+
       {displayed.map((event) => {
         return <Event
           key={event.id}
@@ -167,6 +242,12 @@ export default function Events({ goToFunc, dispatch, map, current }) {
           areaLabel={serviceAreas[event.service_area]?.name}
         />;
       })}
+
+      { search && displayed.length === 0 && (
+        <div className='no-results'>
+          No events match the search and filtering criteria selected.
+        </div>
+      )}
     </div>
   )
 }
