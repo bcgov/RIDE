3c2a69429888f7996f43d043fd7f965a1c316658
ray <ray@oxd.com>
2026-03-30 11:28:52 -0700

    DBC22-6030: fixture update

--- src/webapp/apps/segments/management/commands/purge_segments.py
+++ src/webapp/apps/segments/management/commands/purge_segments.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from apps.segments.models import Route, Segment
+from apps.segments.models import Route, Segment, ChainUp
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Route.objects.all().delete()
         Segment.objects.all().delete()
+        ChainUp.objects.all().delete()
