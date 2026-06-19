cead7ae96bbf1c82275cdd40ead8e82ddca69987
ray <ray@oxd.com>
2026-06-19 15:56:11 -0700

    DBC22-6491: removed unused route

--- src/frontend/app/routes.js
+++ src/frontend/app/routes.js
@@ -3,10 +3,6 @@ import { index, layout, prefix, route } from "@react-router/dev/routes";
 export default [
   layout("./layout.jsx", [
     index("home/home.jsx"),
-    route("cameras", "./cameras/layout.jsx", [
-      index("./cameras/home.jsx"),
-      route("test", "./cameras/test.jsx"),
-    ]),
     route("events", "./events/layout.jsx", [
       index("./events/home.jsx"),
       route("test", "./events/test.jsx"),
