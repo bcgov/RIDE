ea8e9e45c9ad730d40da104179f0f9a2616b9482
ray <ray@oxd.com>
2026-06-25 17:01:07 -0700

    DBC22-6560: refactoring, added new abbreviations to replace

--- src/frontend/app/components/shared/helper.js
+++ src/frontend/app/components/shared/helper.js
@@ -1,21 +1,47 @@
-// Replace all abbreviations in road names
+const ROAD_ABBREVIATIONS = {
+  '\\bHwy\\b': 'Highway',
+  '\\bAve\\b': 'Avenue',
+  '\\bRd\\.(?=\\s|$)': 'Road',
+  '\\bRd\\b(?!\\.)': 'Road',
+  '\\bPl\\b': 'Place',
+  '\\bCrt\\b': 'Court',
+  '\\bBlvd\\b': 'Boulevard',
+  '\\bDr\\b': 'Drive',
+  '\\bPky\\b': 'Parkway',
+  '\\bPkwy\\.(?=\\s|$)': 'Parkway',
+  '\\bPkwy\\b(?!\\.)': 'Parkway',
+  '\\bCres\\b': 'Crescent',
+  '\\bCir\\b': 'Circle',
+  '\\bTerr\\b': 'Terrace',
+  '\\bSt\\.(?=\\s|$)': 'Street',
+  '\\bSt\\b(?!\\.)': 'Street',
+  '\\bLn\\b': 'Lane',
+  '\\bFSR\\b': 'Forest Service Road',
+  '\\bRte\\.(?=\\s|$)': 'Route',
+  '\\bRte\\b(?!\\.)': 'Route',
+  '\\bJunc\\.(?=\\s|$)': 'Junction',
+  '\\bJunc\\b(?!\\.)': 'Junction',
+  '\\bS/B\\b': 'Southbound',
+  '\\bN/B\\b': 'Northbound',
+  '\\bW/B\\b': 'Westbound',
+  '\\bE/B\\b': 'Eastbound',
+  '\\bO/P\\b': 'Overpass',
+  '\\bU/P\\b': 'Underpass',
+  '\\bO/H\\b': 'Overhead',
+  '\\bkmhr\\b': 'km/hr',
+  '\\bkm/h\\b': 'km/hr',
+  '\\bNo\\.(?=\\s|$)': 'Number',
+  '\\bNo\\b(?!\\.)': 'Number',
+};
+
 export const transform_road_abbreviations = (input) => {
   if (typeof input !== 'string') {
     return input;
   }
 
-  return input
-    .replace(/\bHwy\b/g, 'Highway')
-    .replace(/\bAve\b/g, 'Avenue')
-    .replace(/\bRd\b/g, 'Road')
-    .replace(/\bPl\b/g, 'Place')
-    .replace(/\bCrt\b/g, 'Court')
-    .replace(/\bBlvd\b/g, 'Boulevard')
-    .replace(/\bDr\b/g, 'Drive')
-    .replace(/\bPky\b/g, 'Parkway')
-    .replace(/\bCres\b/g, 'Crescent')
-    .replace(/\bCir\b/g, 'Circle')
-    .replace(/\bTerr\b/g, 'Terrace')
-    .replace(/\bSt\b/g, 'Street')
-    .replace(/\bLn\b/g, 'Lane');
+  let s = input;
+  for (const [pattern, fullName] of Object.entries(ROAD_ABBREVIATIONS)) {
+    s = s.replace(new RegExp(pattern, 'g'), fullName);
+  }
+  return s;
 };
