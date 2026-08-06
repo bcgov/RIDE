5dd5c19c5cec10575f5a93aafcf399d4a6e8cebe
Justin Johnson <justin@oxd.com>
2026-08-06 12:45:37 -0700

    DBC22-6992 Two clear buttons

--- src/frontend/app/events/forms/Location/index.jsx
+++ src/frontend/app/events/forms/Location/index.jsx
@@ -3,7 +3,6 @@ import 'react';
 import Tabs from '../../../shared/Tabs';
 import Tooltip from '../../Tooltip';
 
-import { getNearby } from './helpers';
 import Point from './Point';
 
 export default function Location({ errors, event, dispatch, goToFunc, map }) {
